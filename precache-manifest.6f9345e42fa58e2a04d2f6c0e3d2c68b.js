e5937e5d667d69e640c12490b05e52b3c3b1d569
Tisho1991 <tihomir.bogoslovov@adentis.pt>
2021-04-01 20:53:46 +0100

    Updates

---
+++ precache-manifest.6f9345e42fa58e2a04d2f6c0e3d2c68b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4576d237eac287b56cb3c636e5775c3",
+    "url": "/state-hook/index.html"
+  },
+  {
+    "revision": "7ec2532ab00a209d652a",
+    "url": "/state-hook/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "2f011091034c47648cfe",
+    "url": "/state-hook/static/css/main.1f09007f.chunk.css"
+  },
+  {
+    "revision": "7ec2532ab00a209d652a",
+    "url": "/state-hook/static/js/2.afb293b4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/state-hook/static/js/2.afb293b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f011091034c47648cfe",
+    "url": "/state-hook/static/js/main.f216ee13.chunk.js"
+  },
+  {
+    "revision": "8a2480b4309dfdbd5d13",
+    "url": "/state-hook/static/js/runtime-main.776c3391.js"
+  }
+]);
